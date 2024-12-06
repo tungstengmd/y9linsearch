48ae7eb4e04cfd94f92cff0317131231fb9a976b
Harlow 3 <22BKarolia@heckgrammar.co.uk>
2024-12-06 15:15:09 +0000

    yay! ^w^

--- y9linsearch/Program.cs
+++ y9linsearch/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] ints = [1, 2, 3, 4, 5];
+Console.Write("Enter search key: ");
+var searchKey = Convert.ToInt32(Console.ReadLine());
+bool found = false;
+for (int index = 0; index < ints.Length; index++)
+{
+    if (searchKey == ints[index])
+    {
+        found = true;
+        Console.WriteLine("Found!");
+    }
+    if (!found)
+    {
+        Console.WriteLine("Not found. :(");
+    }
+}
